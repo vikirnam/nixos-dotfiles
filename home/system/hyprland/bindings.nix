106aad7daf61021463f8b267dffdf17d585a20ea
vikirna <vikirna@proton.me>
2026-06-16 17:41:18 +0545

    remove unnecessary keybinds
    Signed-off-by: vikirna <vikirna@proton.me>

--- home/system/hyprland/bindings.nix
+++ home/system/hyprland/bindings.nix
@@ -3,171 +3,155 @@
   lib,
   config,
   ...
-}: let
+}:
+let
   colors = config.lib.stylix.colors;
 
-  mkMenu = menu: let
-    configFile = pkgs.writeText "config.yaml" (
-      lib.generators.toYAML {} {
-        anchor = "bottom-right";
-        border = "#${colors.base0D}80";
-        background = "#${colors.base01}EE";
-        color = "#${colors.base05}";
-        margin_right = 15;
-        margin_bottom = 15;
-        rows_per_column = 5;
+  mkMenu =
+    menu:
+    let
+      configFile = pkgs.writeText "config.yaml" (
+        lib.generators.toYAML { } {
+          anchor = "bottom-right";
+          border = "#${colors.base0D}80";
+          background = "#${colors.base01}EE";
+          color = "#${colors.base05}";
+          margin_right = 15;
+          margin_bottom = 15;
+          rows_per_column = 5;
 
-        inherit menu;
-      }
-    );
-  in
+          inherit menu;
+        }
+      );
+    in
     pkgs.writeShellScriptBin "menu" ''
       exec ${lib.getExe pkgs.wlr-which-key} ${configFile}
     '';
-in {
+in
+{
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
     "$shiftMod" = "SUPER_SHIFT";
 
-    bind =
-      [
-        # Applications
-        (
-          "$shiftMod, A, exec, "
-          + lib.getExe (mkMenu [
-            {
-              key = "a";
-              desc = "Proton Authenticator";
-              cmd = "env WEBKIT_DISABLE_COMPOSITING_MODE=1 ${pkgs.proton-authenticator}/bin/proton-authenticator";
-            }
-            {
-              key = "p";
-              desc = "Proton Pass";
-              cmd = "${pkgs.proton-pass}/bin/proton-pass";
-            }
-            {
-              key = "v";
-              desc = "Proton VPN";
-              cmd = "${pkgs.proton-vpn}/bin/protonvpn-app";
-            }
-            {
-              key = "c";
-              desc = "Proton Calendar";
-              cmd = "${config.programs.helium.package}/bin/helium 'https://calendar.proton.me/'";
-            }
-            {
-              key = "m";
-              desc = "Proton Mail";
-              cmd = "${config.programs.helium.package}/bin/helium 'https://mail.proton.me/'";
-            }
-            {
-              key = "o";
-              desc = "Obsidian";
-              cmd = "${pkgs.obsidian}/bin/obsidian";
-            }
-            {
-              key = "s";
-              desc = "Signal";
-              cmd = "${pkgs.signal-desktop}/bin/signal-desktop";
-            }
-            {
-              key = "t";
-              desc = "TickTick";
-              cmd = "${pkgs.ticktick}/bin/ticktick";
-            }
-            {
-              key = "b";
-              desc = "Helium";
-              cmd = "${config.programs.helium.package}/bin/helium";
-            }
-            {
-              key = "i";
-              desc = "Helium (Incognito)";
-              cmd = "${config.programs.helium.package}/bin/helium --incognito";
-            }
-          ])
-        )
+    bind = [
+      # Applications
+      (
+        "$shiftMod, A, exec, "
+        + lib.getExe (mkMenu [
+          {
+            key = "v";
+            desc = "Proton VPN";
+            cmd = "${pkgs.proton-vpn}/bin/protonvpn-app";
+          }
+          {
+            key = "m";
+            desc = "Proton Mail";
+            cmd = "${config.programs.helium.package}/bin/helium 'https://mail.proton.me/'";
+          }
+          {
+            key = "o";
+            desc = "Obsidian";
+            cmd = "${pkgs.obsidian}/bin/obsidian";
+          }
+          {
+            key = "s";
+            desc = "Signal";
+            cmd = "${pkgs.signal-desktop}/bin/signal-desktop";
+          }
+          {
+            key = "b";
+            desc = "Helium";
+            cmd = "${config.programs.helium.package}/bin/helium";
+          }
+          {
+            key = "i";
+            desc = "Helium (Incognito)";
+            cmd = "${config.programs.helium.package}/bin/helium --incognito";
+          }
+        ])
+      )
 
-        "$mod,B, exec, uwsm app -- ${config.programs.helium.package}/bin/helium" # Browser
+      "$mod,B, exec, uwsm app -- ${config.programs.helium.package}/bin/helium" # Browser
 
-        # Power
-        "$mod, X, global, caelestia:session" # Powermenu
-        (
-          "$shiftMod, X, exec, "
-          + lib.getExe (mkMenu [
-            {
-              key = "l";
-              desc = "Lock";
-              cmd = "hyprctl dispatch global caelestia:lock";
-            }
-            {
-              key = "s";
-              desc = "Suspend";
-              cmd = "systemctl suspend";
-            }
-            {
-              key = "r";
-              desc = "Reboot";
-              cmd = "systemctl reboot";
-            }
-            {
-              key = "p";
-              desc = "Power Off";
-              cmd = "systemctl poweroff";
-            }
-            {
-              key = "n";
-              desc = "Nightshift";
-              cmd = "nightshift-toggle";
-            }
-            {
-              key = "c";
-              desc = "Restart caelestia";
-              cmd = "hyprctl dispatch exec 'caelestia-shell kill | sleep 1 | caelestia-shell'";
-            }
-          ])
-        )
+      # Power
+      "$mod, X, global, caelestia:session" # Powermenu
+      (
+        "$shiftMod, X, exec, "
+        + lib.getExe (mkMenu [
+          {
+            key = "l";
+            desc = "Lock";
+            cmd = "hyprctl dispatch global caelestia:lock";
+          }
+          {
+            key = "s";
+            desc = "Suspend";
+            cmd = "systemctl suspend";
+          }
+          {
+            key = "r";
+            desc = "Reboot";
+            cmd = "systemctl reboot";
+          }
+          {
+            key = "p";
+            desc = "Power Off";
+            cmd = "systemctl poweroff";
+          }
+          {
+            key = "n";
+            desc = "Nightshift";
+            cmd = "nightshift-toggle";
+          }
+          {
+            key = "c";
+            desc = "Restart caelestia";
+            cmd = "hyprctl dispatch exec 'caelestia-shell kill | sleep 1 | caelestia-shell'";
+          }
+        ])
+      )
 
-        # Quick launch
-        "$mod,RETURN, exec, uwsm app -- ${pkgs.ghostty}/bin/ghostty" # Ghostty (terminal)
-        "$mod,E, exec,  uwsm app -- ${pkgs.thunar}/bin/thunar" # Thunar
-        "$shiftMod, E, exec, pkill fuzzel || caelestia emoji -p" # Emoji picker
-        "$mod, SPACE, global, caelestia:launcher" # Launcher
-        "$mod, N, exec, caelestia shell drawers toggle sidebar" # Sidebar (Notifications, quick actions)
-        "$mod, D, exec, caelestia shell drawers toggle dashboard" # Dashboard
+      # Quick launch
+      "$mod,RETURN, exec, uwsm app -- ${pkgs.ghostty}/bin/ghostty" # Ghostty (terminal)
+      "$mod,E, exec,  uwsm app -- ${pkgs.thunar}/bin/thunar" # Thunar
+      "$shiftMod, E, exec, pkill fuzzel || caelestia emoji -p" # Emoji picker
+      "$mod, SPACE, global, caelestia:launcher" # Launcher
+      "$mod, N, exec, caelestia shell drawers toggle sidebar" # Sidebar (Notifications, quick actions)
+      "$mod, D, exec, caelestia shell drawers toggle dashboard" # Dashboard
 
-        # Windows
-        "$mod,Q, killactive," # Close window
-        "$mod,F, fullscreen" # Toggle Fullscreen
-        "$shiftMod,F, togglefloating," # Toggle Floating
+      # Windows
+      "$mod,Q, killactive," # Close window
+      "$mod,F, fullscreen" # Toggle Fullscreen
+      "$shiftMod,F, togglefloating," # Toggle Floating
 
-        # Focus Windows
-        "$mod,H, movefocus, l" # Move focus left
-        "$mod,J, movefocus, d" # Move focus Down
-        "$mod,K, movefocus, u" # Move focus Up
-        "$mod,L, movefocus, r" # Move focus Right
-        "$shiftMod,H, focusmonitor, -1" # Focus previous monitor
-        "$shiftMod,J, layoutmsg, removemaster" # Remove from master
-        "$shiftMod,K, layoutmsg, addmaster" # Add to master
-        "$shiftMod,L, focusmonitor, 1" # Focus next monitor
+      # Focus Windows
+      "$mod,H, movefocus, l" # Move focus left
+      "$mod,J, movefocus, d" # Move focus Down
+      "$mod,K, movefocus, u" # Move focus Up
+      "$mod,L, movefocus, r" # Move focus Right
+      "$shiftMod,H, focusmonitor, -1" # Focus previous monitor
+      "$shiftMod,J, layoutmsg, removemaster" # Remove from master
+      "$shiftMod,K, layoutmsg, addmaster" # Add to master
+      "$shiftMod,L, focusmonitor, 1" # Focus next monitor
 
-        # Utilities
-        "$shiftMod, SPACE, exec, caelestia shell gameMode toggle" # Toggle Focus/Game mode
-        "$shiftMod, S, global, caelestia:screenshotFreeze" # Capture region (freeze)
-        ", Print, global, caelestia:screenshotFreeze" # Capture region (freeze)
-        "$shiftMod+Alt, S, global, caelestia:screenshot" # Capture region
-      ]
-      ++ (builtins.concatLists (
-        builtins.genList (
-          i: let
-            ws = i + 1;
-          in [
-            "$mod,code:1${toString i}, workspace, ${toString ws}"
-            "$mod SHIFT,code:1${toString i}, movetoworkspace, ${toString ws}"
-          ]
-        )
-        9
-      ));
+      # Utilities
+      "$shiftMod, SPACE, exec, caelestia shell gameMode toggle" # Toggle Focus/Game mode
+      "$shiftMod, S, global, caelestia:screenshotFreeze" # Capture region (freeze)
+      ", Print, global, caelestia:screenshotFreeze" # Capture region (freeze)
+      "$shiftMod+Alt, S, global, caelestia:screenshot" # Capture region
+    ]
+    ++ (builtins.concatLists (
+      builtins.genList (
+        i:
+        let
+          ws = i + 1;
+        in
+        [
+          "$mod,code:1${toString i}, workspace, ${toString ws}"
+          "$mod SHIFT,code:1${toString i}, movetoworkspace, ${toString ws}"
+        ]
+      ) 9
+    ));
 
     bindm = [
       "$mod,mouse:272, movewindow" # Move Window (mouse)
